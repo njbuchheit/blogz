20c31fce022ade46f9d2b2bd2772204e7bee8e14
Nate Buchheit <njbuchheit@gmail.com>
2016-09-19 18:04:30 -0500

    Finished assignment

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ class BlogHandler(webapp2.RequestHandler):
         """
 
         # TODO - filter the query so that only posts by the given user
-        query = Post.all().filter("author", username).order('-created')
-        return query.fetch(user=user, limit=limit, offset=offset)
+        query = Post.all().filter("author", user).order('-created')
+        return query.fetch(limit=limit, offset=offset)
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
